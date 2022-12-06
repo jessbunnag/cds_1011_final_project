524c980d4217b78f8c292b9455cc6a1d9fa14692
Jess Bunnag <tb2817@nyu.edu>
2022-12-06 18:12:30 -0500

    clean up decoder and add comments

--- decoder.py
+++ decoder.py
@@ -8,44 +8,36 @@ class AttentionModule(nn.Module):
         super(AttentionModule, self).__init__()
         self.l1 = nn.Linear(hidden_dim*2, hidden_dim, bias=False)
 
-    def forward(self, hidden, encoder_outs, src_lens):
+    def forward(self, hidden, encoder_outs):
+        """
+        hidden:         [batch_size, 1, hidden_dim]
+        encoder_outs:   output from encoder (contains last hidden state of each token) 
+                        [batch_size, L, hidden_dim*2]
+        """
         # print(f"===ATTENTION FORWARD===")
         # print(f"hidden shape {hidden.shape}")
         # print(f"encoder_outs shape {encoder_outs.shape}")
 
+        # transform encoder outputs to correct dimension
         att_score = self.l1(encoder_outs) #[128, 600, L]
+
         # print(f"l1(encoder_outs) transpose shape {att_score.transpose(1,2).shape}")
+
+        # calculate each token's h_T W b_i
         att_score = torch.bmm(hidden, att_score.transpose(1,2)) #[128, 1, L]
+
         # print(f"att_score shape {att_score.shape}")
 
+        # calcualte each token's attention score a_i
         attn_scores = F.softmax(att_score.squeeze(), dim=1).unsqueeze(2)
+
         # print(f"att_score after softmax shape {attn_scores.shape}")
-
-        # TODO: confirm whether we need sequence mask 
-
         # print(f"encoder_outs transpose shape {encoder_outs.transpose(1, 2).shape}")
+
+        # calculate context vector from attention scores
         context = torch.bmm(encoder_outs.transpose(1, 2), attn_scores)
         # print(f'context shape {context.shape}')
         return context.squeeze(), attn_scores.squeeze()
-
-        # OLD IMPLEMENTATION 
-        # x = self.l1(hidden)
-        # print(f'x unsqueeze {x.unsqueeze(-1).shape}')
-        # att_score = torch.bmm(encoder_outs, x.unsqueeze(-1)); #this is bsz x seq x 1
-        # att_score = att_score.squeeze(-1); #this is bsz x seq
-        # att_score = att_score.transpose(0, 1)
-        
-        # seq_mask = self.sequence_mask(src_lens, 
-        #                             max_len=max(src_lens).item(), 
-        #                             device = hidden.device).transpose(0, 1)
-
-
-        # masked_att = seq_mask * att_score
-        # masked_att[masked_att == 0] = -1e10
-        # attn_scores = F.softmax(masked_att, dim=0)
-        # x = (attn_scores.unsqueeze(2) * encoder_outs.transpose(0, 1)).sum(dim=0)
-        # x = torch.tanh(self.l2(torch.cat((x, hidden), dim=1)))
-        # return x, attn_scores
 
     def sequence_mask(self, sequence_length, max_len=None, device = torch.device('cuda')):
         if max_len is None:
@@ -79,16 +71,20 @@ class AttnLSTMDecoder(nn.Module):
         
         self.encoder_attention_module = AttentionModule(self.hidden_size)
         
-    def forward(self, input, encoder_outs, hidden_init, targets_len):
+    def forward(self, input, encoder_outs, hidden_init):
         # print(f'===DECODER FORWARD===')
         # print(f'input shape {input.shape}') 
+
+        # pass input (question) through embedding layer
         embedded = self.embedding(input)
 
+        # initial cell state as the same shape as hidden state
         cell_init = torch.zeros_like(hidden_init)
         # print(f'embedded shape {embedded.shape}') 
         # print(f'hidden_init {hidden_init.shape}')
         # print(f'cell_init {cell_init.shape}')
 
+        # pass the embedded input into LSTM to get the hidden states from decoder
         output, (h_out, _) = self.lstm(embedded, (hidden_init,cell_init)) 
         # print(f'output shape {output.shape}') 
         # print(f'h_out shape {h_out.shape}')
@@ -101,21 +97,22 @@ class AttnLSTMDecoder(nn.Module):
         # for each time step (of target) : this should be the target length of the batched targets 
         for t in range(T):
             # compute attention and c_t 
-            # input = hidden, b_i enc_output 
+            # input = hidden, b_i (enc_output) 
             h_t = output[:, t, :]
             # print(f'h_t shape {h_t.shape}')
-            c_t, attn_scores = self.encoder_attention_module(h_t.unsqueeze(1), encoder_outs, targets_len)
+            c_t, attn_scores = self.encoder_attention_module(h_t.unsqueeze(1), encoder_outs)
             # print(f'c_t shape {c_t.shape}')
             # print(f'attn_scores {attn_scores.shape}')
 
-            # concat c_t and h_t
+            # concat context and hidden state to create an input for feedforward net
             h_t_c_t = torch.cat([h_t, c_t], dim=1) 
             # print(f'h_t_c_t {h_t_c_t.shape}')
         
-            # pass concat of c_t;h_t into linear --> tanh --> linear --> softmax 
             fc_out = self.l2(torch.tanh(self.l1(h_t_c_t)))
             # print(f'fc_out {fc_out.shape}')
-            log_prob = F.log_softmax(fc_out,  dim=1) # --> [0, 1, .., 28K] --> [0.2, 0.8, ..., 0.5]
+
+            # get log probability over the classes of all words in the vocab
+            log_prob = F.log_softmax(fc_out,  dim=1) 
             # print(f'log_prob shape {log_prob.shape}')
             # print(f'log_prob {log_prob}')
 
@@ -131,6 +128,5 @@ class AttnLSTMDecoder(nn.Module):
 
         attn_scores_mat = torch.stack(attn_scores_list, dim=2)
 
-        # TODO: return other variables 
         return log_probs_tensor, h_out, attn_scores_mat
         # return return_scores.transpose(0, 1).contiguous(), memory.transpose(0,1), attn_wts_list, context_vec
--- seq2seq.py
+++ seq2seq.py
@@ -16,11 +16,10 @@ class Seq2Seq(nn.Module):
         # calculate sentence encoder's output for init decoder hidden
         # concat last hidden state of forward and backward pass 
         enc_out_repr = torch.cat([
-            enc_hidden[-2, :, :].unsqueeze(0), 
-            enc_hidden[-1, :, :].unsqueeze(0)
+            enc_hidden[-2, :, :].unsqueeze(0),  # last layer of forward pass 
+            enc_hidden[-1, :, :].unsqueeze(0)   # last layer of backward pass
         ], dim=0) # "s" in the paper
-        # print(f'enc_out_repr {enc_out_repr.shape}')
         
-        dec_log_probs, dec_hidden, attn_scores_mat = self.dec(input=question, encoder_outs=enc_output, hidden_init=enc_out_repr, targets_len=src_lens)
+        dec_log_probs, dec_hidden, attn_scores_mat = self.dec(input=question, encoder_outs=enc_output, hidden_init=enc_out_repr)
         
         return dec_log_probs, dec_hidden, attn_scores_mat
